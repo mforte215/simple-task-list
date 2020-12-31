7161dd18c1b385b6d622c9519df69f691127802f
MForte101 <mark.fortee@gmail.com>
2020-12-31 13:04:40 -0500

    fix

--- src/components/ToDoList/todo/Todo.js
+++ src/components/ToDoList/todo/Todo.js
@@ -4,12 +4,9 @@ import classes from './Todo.module.css';
 
 const todo = (props) => {
 
-
-    let taskNumber = parseInt(props.id) + 1;
-
     return (
         <div className={classes.Todo}>
-            <p>{taskNumber}. {props.taskText}</p><button onClick={() => props.click(props.id)}>Delete</button>
+            <p>{props.id}. {props.taskText}</p><button onClick={() => props.click(props.id)}>Delete</button>
         </div>
     );
 }
